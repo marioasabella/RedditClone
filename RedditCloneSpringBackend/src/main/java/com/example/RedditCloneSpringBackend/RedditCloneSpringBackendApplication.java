d584d2f617ab126c31de84e658d6852a795af400
marioasabella <mario.asabella@gmail.com>
2020-11-22 17:43:53 +0100

    Initialized spring boto backend

---
+++ RedditCloneSpringBackend/src/main/java/com/example/RedditCloneSpringBackend/RedditCloneSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedditCloneSpringBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneSpringBackendApplication.class, args);
+	}
+
+}
