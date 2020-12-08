c3fb1bf5b39f1302d896303b5c2256a570fc71a7
marioasabella <mario.asabella@gmail.com>
2020-12-08 14:33:04 +0100

    Added auth service, Mail content builder and template

---
+++ RedditCloneSpringBackend/src/main/java/com/example/RedditCloneSpringBackend/service/AuthService.java
@@ -0,0 +1,48 @@
+package com.example.RedditCloneSpringBackend.service;
+
+import com.example.RedditCloneSpringBackend.dto.RegisterRequest;
+import com.example.RedditCloneSpringBackend.model.User;
+import com.example.RedditCloneSpringBackend.model.VerificationToken;
+import com.example.RedditCloneSpringBackend.repository.UserRepository;
+import com.example.RedditCloneSpringBackend.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
+    }
+}
